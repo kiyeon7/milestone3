cadc1c85031e64441dafd0d28487d7019721e03f
Kiyeon Lee <16848778+kiyeon7@users.noreply.github.com>
2021-02-26 19:14:01 -0500

    basics of milestone3 done

---
+++ milestone3/app.R
@@ -0,0 +1,51 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+ui <- navbarPage(
+    "Final Project Title",
+    tabPanel("Model",
+             fluidPage(
+                 titlePanel("Model Title"),
+                 sidebarLayout(
+                     sidebarPanel(
+                         selectInput(
+                             "plot_type",
+                             "Plot Type",
+                             c("Option A" = "a", "Option B" = "b")
+                         )),
+                     mainPanel(plotOutput("line_plot")))
+             )),
+    tabPanel("Discussion",
+             titlePanel("Discussion Title"),
+             p("Tour of the modeling choices you made and 
+              an explanation of why you made them")),
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Data sources I plan to use are https://github.com/notpeter/crunchbase-data and https://www.kauffman.org/entrepreneurship/research/data-resources/"),
+             h3("About Me"),
+             p("https://github.com/kiyeon7/milestone3.git   https://kiyeon.shinyapps.io/milestone3/")))
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
